fd946c7297747a2391601db4c67ecf53be23696e
Guido <pereyraguido@gmail.com>
2021-08-04 21:28:03 -0300

    Create mario_v1.c

---
+++ week1/mario_v1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void print_level(int);
+
+int main(void)
+{
+    int level_num;
+    do
+    {
+        level_num = get_int("Insert how much levels you would like\n");
+
+    }
+    while(level_num<1 || level_num>8);
+
+    print_level(level_num);
+
+}
+
+void print_level(int n)
+{
+    for(int x = 0; x<n ; x++)
+    {
+
+        for(int y = 0; y<=x; y++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
