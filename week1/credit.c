9e4042970e9bc66a210d8a49803cd8e646d7dc4f
Guido <pereyraguido@gmail.com>
2021-08-07 17:36:32 -0300

    adding checksum

--- week1/credit.c
+++ week1/credit.c
@@ -13,6 +13,9 @@ int main(void)
     long num = 1;
     int count = 0;
     long credit_card_prefix = 0;
+    int first_checksum_sum = 0;
+    int second_checksum_sum = 0;
+    int extra_num=0;
 
     // getting digit quantity and prefix
     // the idea is to divide the credit card number
@@ -20,6 +23,33 @@ int main(void)
     for(long x =10; num>0; x = x*10)
     {
         num = credit_card_number / x;
+
+        //calculating check_sum
+        // if it is the first iteration i save the first and the second digit
+        if(count == 0){
+            second_checksum_sum += credit_card_number%10;
+            first_checksum_sum += (num%10)*2;
+        }else
+        {
+            // if it is not the first iteration,
+            // and the iteration is not even,
+            // i calculate the second sum
+            if(count%2 != 0){
+                second_checksum_sum += num%10;
+            }else{
+                // if the iteration is even,
+                // i calculate the first sum multiplying by 2
+                extra_num = (num%10)*2;
+                // if the result is greater than 10,
+                // i split the digit before summarize
+                if (extra_num>9)
+                {
+                    first_checksum_sum += ((int) extra_num%10 + (int) extra_num/10);
+                }else{
+                    first_checksum_sum += (int) extra_num ;
+                }
+            }
+        }
 
         count +=1;
 
@@ -40,19 +70,25 @@ int main(void)
         }
     }
 
-    // Checking credit card brand
-    if(count == 15 && (credit_card_prefix  == 34 || credit_card_prefix ==37))
-    {
-        printf("AMEX\n");
-    }else if((count == 13 || count == 16) && (credit_card_prefix/10) == 4 )
-    {
-        printf("VISA\n");
-    }else if((count == 16) && (credit_card_prefix >= 51 && credit_card_prefix<=55))
-    {
-        printf("MASTER\n");
+    int check_sum = (int )(first_checksum_sum + second_checksum_sum) %10;
 
-    }else{
+    if (check_sum != 0){
         printf("INVALID\n");
+    }else{
+        // Checking credit card brand
+        if(count == 15 && (credit_card_prefix  == 34 || credit_card_prefix ==37))
+        {
+            printf("AMEX\n");
+        }else if((count == 13 || count == 16) && (credit_card_prefix/10) == 4 )
+        {
+            printf("VISA\n");
+        }else if((count == 16) && (credit_card_prefix >= 51 && credit_card_prefix<=55))
+        {
+            printf("MASTER\n");
+
+        }else{
+            printf("INVALID\n");
+        }
     }
 
 }
